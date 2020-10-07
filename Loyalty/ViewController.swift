44822acad4f5b359039bdf7c28836ebbbbbe45be
treCoops <trevogayan1995@gmail.com>
2020-10-07 19:40:22 +0530

    Initial Commit

---
+++ Loyalty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loyalty
+//
+//  Created by treCoops on 10/7/20.
+//  Copyright © 2020 treCoops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
